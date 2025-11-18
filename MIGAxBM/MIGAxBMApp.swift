c36c8960bc3632f07ab1e1fea7f3afdd4fbca448
Doughy <Doughyvibe@gmail.com>
2025-11-18 17:55:47 +0800

    Initial Commit

---
+++ MIGAxBM/MIGAxBMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIGAxBMApp.swift
+//  MIGAxBM
+//
+//  Created by Doughy on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MIGAxBMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MIGAxBMTests/MIGAxBMTests.swift
@@ -0,0 +1,17 @@
+//
+//  MIGAxBMTests.swift
+//  MIGAxBMTests
+//
+//  Created by Doughy on 18/11/25.
+//
+
+import Testing
+@testable import MIGAxBM
+
+struct MIGAxBMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
